a2a096bead76ea85136a27932f5be593c0ccf749
evkim1019 <kimgem89@gmail.com>
2021-09-13 07:28:46 -0400

    employee landing page db linked

--- client/components/EmployeeLanding.js
+++ client/components/EmployeeLanding.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { getStudents, getUsers } from '../store';
+import { getStudents } from '../store';
 
 /**
  * COMPONENT
  */
-class AdminDashboard extends Component {
+class EmployeeLanding extends Component {
   constructor(props) {
     super(props)
   }
 
   async componentDidMount() {
     await this.props.getStudents();
-    await this.props.getUsers();
 
-    // this.setState({ users: this.props.users });
-    // this.setState({ students: this.props.students })
   }
 
   render() {
@@ -28,7 +25,7 @@ class AdminDashboard extends Component {
         </div>
         <div className="col2">
           <div className="block sidepanel">
-            <h2>School Name</h2>
+            <h2>Hi, Teacher {this.props.username}</h2>
             <form>
               <label>school location</label>
               <select>
@@ -84,19 +81,21 @@ class AdminDashboard extends Component {
    * CONTAINER
    */
 
+// const mapState = state => {
+//   return {
+//     username: state.username
+//   }
+// }
 const mapStateToProps = state => {
   return {
-    // username: state.auth.username
-    users: state.users,
+    username: state.auth.username,
     students: state.students
-    // state
   }
 }
 
 
 const mapDispatchToProps = {
   getStudents,
-  getUsers,
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(AdminDashboard)
+export default connect(mapStateToProps, mapDispatchToProps)(EmployeeLanding)
