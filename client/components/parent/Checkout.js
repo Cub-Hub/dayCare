720f0f74e645539f7be1d515c6cba5ed74d68d7a
heinnssin <heinnssinp@gmail.com>
2021-09-23 20:48:04 -0400

    optimize checkout component

--- client/components/parent/Checkout.js
+++ client/components/parent/Checkout.js
@@ -6,42 +6,33 @@ const Checkout = (props) =>{
   const { auth } = props
 
   const handleSingleSubmit = async (evt) =>{
-    evt.preventDefault()
     const { data } = await axios.post('/api/create-checkout-session/')
     window.location = data.url
   }
-  const handleSubscriptionSubmit = async (evt) =>{
-    evt.preventDefault()
+  const handleSubscriptionSubmit = async () =>{
     const { data } = await axios.post('/api/create-checkout-session/subscription', {lookup_keys: '1subscription'})
-    console.log('subscription data--->', data)
     await axios.post(`/api/addsession/${auth.id}/${data.sessionId}`)
     window.location = data.url
   }
   
 return (
   <div>
-    <section>
-       <div className="product">
-        <div className="description">
-          <h3>Day Care Standard Plan</h3>
-          <h5>$199.99 / day</h5>
-        </div>
+    <div className="product">
+      <div className="description">
+        <h3>Day Care Standard Plan</h3>
+        <h5>$199.99 / day</h5>
       </div>
-      <form onSubmit={handleSingleSubmit}>
-        <button type="submit">
-          Single Payment
-        </button>
-      </form>
-    </section>
-    <section>
-      <div>
-        <form onSubmit={handleSubscriptionSubmit}>
-          <button type='submit'>
-            Monthly Subscription
-          </button>
-        </form>
-      </div>
-    </section>
+    </div>
+    <div>
+      <button onClick={ ()=> handleSingleSubmit() } >
+        Single Payment
+      </button>
+    </div>
+    <div>
+      <button onClick={ ()=> handleSubscriptionSubmit() } >
+        Monthly Subscription
+      </button>
+    </div>
   </div>
  )
 }
@@ -53,3 +44,28 @@ const mapState =(state) =>{
 }
 
 export default connect(mapState)(Checkout)
+
+{/* <div>
+<section>
+   <div className="product">
+    <div className="description">
+      <h3>Day Care Standard Plan</h3>
+      <h5>$199.99 / day</h5>
+    </div>
+  </div>
+  <form onSubmit={handleSingleSubmit}>
+    <button type="submit">
+      Single Payment
+    </button>
+  </form>
+</section>
+<section>
+  <div>
+    <form onSubmit={handleSubscriptionSubmit}>
+      <button type='submit'>
+        Monthly Subscription
+      </button>
+    </form>
+  </div>
+</section>
+</div> */}
